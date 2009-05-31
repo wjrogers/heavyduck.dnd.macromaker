c3c423d926149e387ec35f7751263bd217547e18
Will Rogers <wjrogers@gmail.com>
2009-05-31 17:18:07 -0400

    Import new project

---
+++ Forms/Main.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+using HeavyDuck.Utilities;
+using HeavyDuck.Utilities.Forms;
+
+namespace HeavyDuck.Dnd.MacroMaker.Forms
+{
+    public partial class Main : Form
+    {
+        protected const string WEAPON_NONE = "NONE";
+        protected const string MACRO_ELEMENT_NAME = "net.rptools.maptool.model.MacroButtonProperties";
+
+        private readonly CompendiumHelper m_compendium = new CompendiumHelper();
+
+        public Main()
+        {
+            InitializeComponent();
+
+            // event handlers
+            this.Load += new EventHandler(Main_Load);
+            this.FormClosed += new FormClosedEventHandler(Main_FormClosed);
+            browse_character_button.Click += new EventHandler(browse_character_button_Click);
+            browse_macro_button.Click += new EventHandler(browse_macro_button_Click);
+            go_button.Click += new EventHandler(go_button_Click);
+        }
+
+        #region Event Handlers
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // let the compendium helper load saved cookies
+            m_compendium.LoadCookies();
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // save cookies
+            m_compendium.SaveCookies();
+        }
+
+        private void browse_character_button_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog d = new OpenFileDialog())
+            {
+                d.CheckFileExists = true;
+                d.Filter = "Character Builder Files (*.dnd4e)|*.dnd4e";
+
+                if (d.ShowDialog(this) == DialogResult.OK)
+                    character_box.Text = d.FileName;
+            }
+        }
+
+        private void browse_macro_button_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog d = new SaveFileDialog())
+            {
+                d.AddExtension = true;
+                d.Filter = "MapTool Macro Sets (*.mtmacset)|*.mtmacset";
+                d.OverwritePrompt = true;
+                d.ValidateNames = true;
+
+                if (d.ShowDialog(this) == DialogResult.OK)
+                    macro_box.Text = d.FileName;
+            }
+        }
+
+        private void go_button_Click(object sender, EventArgs e)
+        {
+            ProgressDialog d = new ProgressDialog();
+            string character_path = character_box.Text;
+            string macro_path = macro_box.Text;
+            List<PowerInfo> powers = new List<PowerInfo>();
+            Dictionary<string, string> power_definitions = new Dictionary<string, string>();
+
+            d.AddTask((progress) =>
+            {
+                progress.Update("Scanning powers...");
+
+                using (FileStream fs = File.OpenRead(character_path))
+                {
+                    XPathDocument doc = new XPathDocument(fs);
+                    XPathNavigator nav = doc.CreateNavigator(), temp_nav;
+                    XPathNodeIterator power_iter, weapon_iter;
+
+                    // select all power nodes
+                    power_iter = nav.Select("/D20Character/CharacterSheet/PowerStats/Power");
+
+                    // update the progress steps
+                    progress.Update(0, power_iter.Count);
+
+                    // iterate them
+                    while (power_iter.MoveNext())
+                    {
+                        string name;
+                        string action_type;
+                        PowerUsage usage;
+
+                        try
+                        {
+                            // read the basic stuff
+                            name = power_iter.Current.SelectSingleNode("@name").Value;
+                            usage = Util.EnumParse<PowerUsage>(power_iter.Current.SelectSingleNode("specific[@name = 'Power Usage']").Value.Replace("-", ""));
+                            action_type = power_iter.Current.SelectSingleNode("specific[@name = 'Action Type']").Value.Trim();
+
+                            // get the url for the power in the compendium and attempt to retrieve it
+                            temp_nav = null; // nav.SelectSingleNode("//RulesElementTally/RulesElement[@name = \"" + name + "\"]/@url");
+                            if (temp_nav != null)
+                            {
+                                HtmlAgilityPack.HtmlDocument scraper_doc = new HtmlAgilityPack.HtmlDocument();
+                                HtmlAgilityPack.HtmlNodeNavigator scraper_result;
+                                XPathNavigator scraper_nav;
+
+                                // slurp
+                                using (Stream s = m_compendium.GetEntryByUrl(temp_nav.Value))
+                                {
+                                    scraper_doc.Load(s);
+                                    scraper_nav = scraper_doc.CreateNavigator();
+                                }
+                                    
+                                // save the content part
+                                scraper_result = scraper_nav.SelectSingleNode("//div[@id = 'detail']") as HtmlAgilityPack.HtmlNodeNavigator;
+                                if (scraper_result != null)
+                                    power_definitions[name] = scraper_result.CurrentNode.OuterHtml;
+                            }
+
+                            // select weapons
+                            weapon_iter = power_iter.Current.Select("Weapon");
+
+                            // some powers aren't attacks or don't use weapons
+                            if (weapon_iter.Count < 1)
+                            {
+                                powers.Add(new PowerInfo(name, WEAPON_NONE, usage, action_type));
+                            }
+                            else
+                            {
+                                while (weapon_iter.MoveNext())
+                                {
+                                    PowerInfo power = new PowerInfo(name, weapon_iter.Current.SelectSingleNode("@name").Value, usage, action_type);
+
+                                    power.AttackBonus = weapon_iter.Current.SelectSingleNode("AttackBonus").ValueAsInt;
+                                    power.AttackStat = Util.EnumParse<Stat>(weapon_iter.Current.SelectSingleNode("AttackStat").Value);
+                                    power.DamageExpression = weapon_iter.Current.SelectSingleNode("Damage").Value.Trim();
+                                    power.Defense = weapon_iter.Current.SelectSingleNode("Defense").Value.Trim();
+
+                                    powers.Add(power);
+                                }
+                            }
+
+
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+
+                        // wait briefly to avoid spamming compendium, then advance progress
+                        System.Threading.Thread.Sleep(200);
+                        progress.Advance();
+                    }
+                }
+            });
+
+            d.AddTask((progress) =>
+            {
+                progress.Update("Writing macros...");
+
+                using (FileStream fs = File.Open(macro_path, FileMode.Create, FileAccess.Write))
+                {
+                    XmlWriterSettings settings;
+
+                    settings = new XmlWriterSettings();
+                    settings.Encoding = new UTF8Encoding(false);
+                    settings.Indent = true;
+                    settings.IndentChars = "  ";
+                    settings.OmitXmlDeclaration = false;
+
+                    using (XmlWriter w = XmlWriter.Create(fs, settings))
+                    {
+                        w.WriteStartDocument();
+                        w.WriteStartElement("list");
+
+                        foreach (PowerInfo power in powers)
+                        {
+                            StringBuilder command = new StringBuilder();
+                            string text, background;
+
+                            // skip basic attacks with no weapons
+                            if (IsPowerBasicAttack(power.Name) && power.Weapon == WEAPON_NONE)
+                                continue;
+
+                            // begin macro definition
+                            w.WriteStartElement(MACRO_ELEMENT_NAME);
+                            w.WriteElementString("saveLocation", "Token");
+                            w.WriteElementString("label", power.Name);
+                            w.WriteElementString("autoExecute", "true");
+                            w.WriteElementString("sortby", GetSortPriority(power));
+
+                            // set colors based on usage limits
+                            if (GetColors(power.Usage, out text, out background))
+                            {
+                                w.WriteElementString("fontColorKey", text);
+                                w.WriteElementString("colorKey", background);
+                            }
+
+                            // start with the table and header
+                            command.AppendLine("<table border=\"0\" width=\"300\">");
+                            command.AppendFormat("<tr bgcolor=\"{0}\" style=\"color: white;\">", background);
+                            command.AppendLine();
+                            command.AppendFormat("<th align=\"left\" style=\"font-size: 1.1em; padding: 2px 4px;\">{0}</th>", power.Name);
+                            command.AppendLine();
+                            command.AppendFormat("<td align=\"right\" valign=\"middle\" style=\"padding: 2px;\">{0}</td></tr>", power.ActionType);
+                            command.AppendLine();
+                            command.AppendLine("</tr>");
+
+                            // when we have a weapon, put the macro in the weapon's group and include the rolls
+                            if (power.Weapon != WEAPON_NONE)
+                            {
+                                w.WriteElementString("group", power.Weapon);
+
+                                command.AppendLine("<tr><td colspan=\"2\"><i>" + power.Weapon + "</i></td></tr>");
+                                command.AppendFormat("<tr><td nowrap><b>[1d20+{0}]</b></td><td><b>vs. {1}</b></td></tr>", power.AttackBonus, power.Defense);
+                                command.AppendLine();
+                                command.AppendFormat("<tr><td nowrap>[{0}]</td><td>damage</td></tr>", power.DamageExpression);
+                                command.AppendLine();
+                            }
+
+                            // finish up the command
+                            command.AppendLine("</table>");
+
+                            // write the command and end element
+                            w.WriteElementString("command", command.ToString());
+                            w.WriteEndElement(); // MACRO_ELEMENT_NAME
+                        }
+
+                        w.WriteEndDocument();
+                    }
+                }
+            });
+
+            try
+            {
+                d.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+
+        private static bool IsPowerBasicAttack(string name)
+        {
+            return name.EndsWith("Basic Attack");
+        }
+
+        private static string GetSortPriority(PowerInfo power)
+        {
+            switch (power.Usage)
+            {
+                case PowerUsage.AtWill:
+                    if (IsPowerBasicAttack(power.Name))
+                        return "09";
+                    else
+                        return "10";
+                case PowerUsage.Encounter:
+                    return "20";
+                case PowerUsage.Daily:
+                    return "30";
+                default:
+                    return "99";
+            }
+        }
+
+        private static bool GetColors(PowerUsage usage, out string text, out string background)
+        {
+            switch (usage)
+            {
+                case PowerUsage.AtWill:
+                    text = "black";
+                    background = "green";
+                    return true;
+                case PowerUsage.Encounter:
+                    text = "white";
+                    background = "red";
+                    return true;
+                case PowerUsage.Daily:
+                    text = "white";
+                    background = "black";
+                    return true;
+                default:
+                    text = "";
+                    background = "";
+                    return false;
+            }
+        }
+
+        protected class PowerInfo
+        {
+            public string Name { get; set; }
+            public string Weapon { get; set; }
+            public PowerUsage Usage { get; set; }
+            public string ActionType { get; set; }
+            public int AttackBonus { get; set; }
+            public Stat AttackStat { get; set; }
+            public string DamageExpression { get; set; }
+            public string Defense { get; set; }
+
+            public PowerInfo(string name, string weapon, PowerUsage usage, string action_type)
+            {
+                this.Name = name;
+                this.Weapon = weapon;
+                this.Usage = usage;
+                this.ActionType = action_type;
+            }
+        }
+
+        protected enum PowerUsage
+        {
+            AtWill,
+            Encounter,
+            Daily
+        }
+
+        protected enum Stat
+        {
+            Strength,
+            Constitution,
+            Dexterity,
+            Intelligence,
+            Wisdom,
+            Charisma
+        }
+    }
+}
---
+++ Forms/Main.designer.cs
@@ -0,0 +1,134 @@
+﻿namespace HeavyDuck.Dnd.MacroMaker.Forms
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.character_box = new System.Windows.Forms.TextBox();
+            this.browse_character_button = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.browse_macro_button = new System.Windows.Forms.Button();
+            this.macro_box = new System.Windows.Forms.TextBox();
+            this.go_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Character File";
+            // 
+            // character_box
+            // 
+            this.character_box.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.character_box.Location = new System.Drawing.Point(12, 25);
+            this.character_box.Name = "character_box";
+            this.character_box.Size = new System.Drawing.Size(407, 20);
+            this.character_box.TabIndex = 1;
+            // 
+            // browse_character_button
+            // 
+            this.browse_character_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browse_character_button.Location = new System.Drawing.Point(425, 23);
+            this.browse_character_button.Name = "browse_character_button";
+            this.browse_character_button.Size = new System.Drawing.Size(75, 23);
+            this.browse_character_button.TabIndex = 2;
+            this.browse_character_button.Text = "Browse...";
+            this.browse_character_button.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Macro File (output)";
+            // 
+            // browse_macro_button
+            // 
+            this.browse_macro_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.browse_macro_button.Location = new System.Drawing.Point(425, 62);
+            this.browse_macro_button.Name = "browse_macro_button";
+            this.browse_macro_button.Size = new System.Drawing.Size(75, 23);
+            this.browse_macro_button.TabIndex = 5;
+            this.browse_macro_button.Text = "Browse...";
+            this.browse_macro_button.UseVisualStyleBackColor = true;
+            // 
+            // macro_box
+            // 
+            this.macro_box.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.macro_box.Location = new System.Drawing.Point(12, 64);
+            this.macro_box.Name = "macro_box";
+            this.macro_box.Size = new System.Drawing.Size(407, 20);
+            this.macro_box.TabIndex = 4;
+            // 
+            // go_button
+            // 
+            this.go_button.Location = new System.Drawing.Point(219, 233);
+            this.go_button.Name = "go_button";
+            this.go_button.Size = new System.Drawing.Size(75, 23);
+            this.go_button.TabIndex = 6;
+            this.go_button.Text = "Go";
+            this.go_button.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 433);
+            this.Controls.Add(this.go_button);
+            this.Controls.Add(this.browse_macro_button);
+            this.Controls.Add(this.macro_box);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.browse_character_button);
+            this.Controls.Add(this.character_box);
+            this.Controls.Add(this.label1);
+            this.Name = "Main";
+            this.Text = "Token Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox character_box;
+        private System.Windows.Forms.Button browse_character_button;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button browse_macro_button;
+        private System.Windows.Forms.TextBox macro_box;
+        private System.Windows.Forms.Button go_button;
+    }
+}
